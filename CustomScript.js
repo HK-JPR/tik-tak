a9486e9744051f730010aa5fb4af4c90ac11ca9a
chatwithh <57766998+chatwithh@users.noreply.github.com>
2020-02-05 22:35:06 +0530

    Add files via upload
    upload latest index and js file

---
+++ CustomScript.js
@@ -0,0 +1,114 @@
+﻿(function ($) {
+    $(function () {
+        var Player1Name = "";
+        var Player2Name = "";
+        var count = 0;
+
+        $("#SetPlayerName").on("click", function () {
+            Player1Name = $("#player1").val().trim();
+            Player2Name = $("#player2").val().trim();
+            SetPlayer();
+        });
+
+
+        $("#RandomName").on("click", function () {
+            Player1Name = "John";
+            Player2Name = "Johnny";
+            SetPlayer();
+        })
+
+
+
+        //For Set Player Name And hide button
+        function SetPlayer() {
+            $("#player1").parent().text("Player 1 :" + Player1Name).addClass("text-danger");
+            $("#player2").parent().text("Player 2 :" + Player2Name).addClass("text-info");;
+            $("#SetPlayerName").hide();
+            $("#RandomName").hide();
+            $("#GreetingText").text(Player1Name + "'s Turn (♪)");
+        }
+
+
+        //Click on any cloumn on grid
+        $("td").on("click", function () {
+            if (Player1Name == "" && Player2Name == "") {
+                alert("Please Enter Players Name for Start Game")
+            }
+            else {
+                var TdClass = $(this).prop("class");
+                if (TdClass == "") {
+                    count += 1;
+                    if (count % 2 == 0) {
+                        $("#GreetingText").text(Player1Name + "'s Turn (♪)");
+                        $(this).addClass("bg-info");
+                        $(this).text("♫")
+                    }
+                    else {
+                        $("#GreetingText").text(Player2Name + "'s Turn (♫)");
+                        $(this).addClass("bg-danger");
+                        $(this).text("♪")
+                    }
+                    var pattern = $(this).prop("class");
+                    var won1 = checkIfPlayerWon(pattern)
+                    if (count == 9) {
+                        if (won1 != "bg-danger") {
+                            won1 = "Tie";
+                        }
+                    }
+                    AlertWon(won1)
+                }
+            }
+        });
+
+
+        //Check Player Winning
+        function checkIfPlayerWon(pattern) {
+            var won = 0;
+            var table = $('table');
+            //Row Wise checking
+            if (table.find('#R1C1').hasClass(pattern) && table.find('#R1C2').hasClass(pattern) && table.find('#R1C3').hasClass(pattern)) {
+                won = pattern;
+            } else if (table.find('#R2C1').hasClass(pattern) && table.find('#R2C2').hasClass(pattern) && table.find('#R2C3').hasClass(pattern)) {
+                won = pattern;
+            } else if (table.find('#R3C1').hasClass(pattern) && table.find('#R3C2').hasClass(pattern) && table.find('#R3C3').hasClass(pattern)) {
+                won = pattern;
+            }
+
+            //Cloumn wise checking
+            else if (table.find('#R1C1').hasClass(pattern) && table.find('#R2C1').hasClass(pattern) && table.find('#R3C1').hasClass(pattern)) {
+                won = pattern;
+            } else if (table.find('#R1C2').hasClass(pattern) && table.find('#R2C2').hasClass(pattern) && table.find('#R3C2').hasClass(pattern)) {
+                won = pattern;
+            } else if (table.find('#R1C3').hasClass(pattern) && table.find('#R2C3').hasClass(pattern) && table.find('#R3C3').hasClass(pattern)) {
+                won = pattern;
+            }
+
+            //Other
+            else if (table.find('#R1C1').hasClass(pattern) && table.find('#R2C2').hasClass(pattern) && table.find('#R3C3').hasClass(pattern)) {
+                won = pattern;
+            } else if (table.find('#R1C3').hasClass(pattern) && table.find('#R2C2').hasClass(pattern) && table.find('#R3C1').hasClass(pattern)) {
+                won = pattern;
+            }
+            return won;
+        }
+
+        //Alert Match Result
+        function AlertWon(winner) {
+            if (winner == "bg-danger") {
+                $("#GreetingText").text(Player1Name + " Won this match ♥");
+                $("td").off("click");
+                setTimeout(function () { location.reload(true); }, 10000);
+            }
+            else if (winner == "bg-info") {
+                $("#GreetingText").text(Player2Name + " Won this match ♥");
+                $("td").off("click");
+                setTimeout(function () { location.reload(true); }, 10000);
+            }
+            else if (winner == "Tie") {
+                $("#GreetingText").text("Match Tie ☺");
+                setTimeout(function () { location.reload(true); }, 10000);
+            }
+        }
+    });
+
+})(jQuery)
